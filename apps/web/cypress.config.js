e37725d6aade4b3aad6446f36568b46ab455d488
delucca <gpg@delucca.dev>
2023-09-24 03:56:43 +0000

    chore: finishes setting up cypress

---
+++ apps/web/cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+    e2e: {
+        baseUrl: 'http://localhost:4200',
+        supportFile: 'e2e/support/e2e.{js,jsx,ts,tsx}',
+        fixturesFolder: 'e2e/fixtures',
+        specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
+    },
+})
--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-  },
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+	},
 };
