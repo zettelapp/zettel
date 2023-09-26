7a8076ed5f2307523c565cab0c735128ee959ec5
delucca <gpg@delucca.dev>
2023-09-26 13:17:51 +0000

    chore: fixing configs

--- apps/web/postcss.config.ts
+++ apps/web/postcss.config.ts
@@ -1,9 +1,14 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import type { AcceptedPlugin } from 'postcss';
+
+type PostCssConfig = {
+  plugins: AcceptedPlugin[];
+};
 
 export default {
   plugins: [
     tailwindcss({ config: './apps/web/tailwind.config.ts' }),
     autoprefixer,
   ],
-};
+} satisfies PostCssConfig;
