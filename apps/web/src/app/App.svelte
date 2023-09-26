b476d81e5c01e87bee13fda135b1ec483fadc1e0
delucca <gpg@delucca.dev>
2023-09-26 14:28:07 +0000

    chore(libs): base structure for ui lib

--- apps/web/src/app/App.svelte
+++ apps/web/src/app/App.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { Avatar } from '@skeletonlabs/skeleton';
+	import { ui } from '@zettel/ui'
+	
+	console.log(ui())
 
     let count: number = 0
     const increment = () => {
@@ -16,13 +19,3 @@
 <button on:click={increment}>
   count is {count}
 </button>
-
-<div class="container mx-auto p-8 space-y-8">
-	<h1 class="h1">Hello Skeleton</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-	<section>
-		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
-		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
-		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
-	</section>
-</div>
--- apps/web/vite.config.mts
+++ apps/web/vite.config.mts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import postcss from './postcss.config';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), nxViteTsPaths()],
 
   server: {
     port: 4200,
